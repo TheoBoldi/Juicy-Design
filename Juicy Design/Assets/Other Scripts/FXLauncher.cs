60e4727b281ebc630d22b74f13d173bf44a89cf7
de <luciendelmas06@gmail.com>
2021-01-08 08:49:19 +0100

    fx party

--- Juicy Design/Assets/Other Scripts/FXLauncher.cs
+++ Juicy Design/Assets/Other Scripts/FXLauncher.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class FXLauncher : MonoBehaviour
 {
     public ParticleSystem[] particles;
+    public ParticleSystem[] particles_Second;
+    public ParticleSystem[] particles_Third;
 
     public void LaunchParticles()
     {
         foreach (ParticleSystem p in particles)
+            p.Play();
+    }
+    public void LaunchParticles_Second()
+    {
+        foreach (ParticleSystem p in particles_Second)
+            p.Play();
+    }
+    public void LaunchParticles_Third()
+    {
+        foreach (ParticleSystem p in particles_Third)
             p.Play();
     }
 
